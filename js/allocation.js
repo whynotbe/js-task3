c36570ea7df2faec4bdc707d139bdf15f6321cc5
whynotbe <1178003866@qq.com>
2017-03-27 15:30:24 +0800

    bugfix

--- js/allocation.js
+++ js/allocation.js
@@ -2,12 +2,11 @@
  * Created by luzhaolin on 2017/3/13.
  */
 window.onload = function(){
-    var a  = getNumber(); //分配杀手组与平民组,并更改数量. 如果输入不合法,弹出提示
-    submit(); //为"去发牌"按钮添加点击事件. 点击转到查看身份-1界面.
+    submit(); //获取数字,分配身份,为"去发牌"按钮添加点击事件. 点击转到查看身份-1界面.
 }
 
 
-function getNumber(){
+function getNumber(){  //获取数字,分配身份,返还身份字符串
     var number = document.getElementById("input");
     
     allocate(number.value);
@@ -17,11 +16,40 @@ function getNumber(){
         window.location.href = "1-main.html";
     }
 
+
+    var inputTimes = 1;
     number.onblur = function(){
         if(parseInt(number.value) == number.value){
             if(number.value>3&&number.value<19)
             {
              allocate(number.value);
+                var civil = []; //建立玩家数组
+                var killers = []; //建立杀手数组,这个数组存放杀手玩家的序号. 另一种方式是建立一个数组,把"杀手""平民"设置为
+                                  // 数组元素的值,然后打乱序号. 在使用上更简洁一些.
+                var num = number.value;
+                var killerNum = Math.floor(num/4);
+                for(var i = 1;i <= num; i++){
+                    civil.push(i);
+                }
+                killers = shuffle(killerNum,civil);
+                console.log("这是第"+inputTimes+"次输入,一共"+num+"名玩家,其中"+killerNum+"名杀手,"+civil.length+"名平民");
+                console.log("杀手的序号是");
+                for( i = 0;i<killers.length;i++)
+                {
+                    var a = Number(killers[i]);
+                    console.log(a);
+                }
+                console.log("平民的序号是");
+                for( i = 0;i<civil.length;i++)
+                {
+                    console.log(civil[i]);
+                }
+
+                var id = killers.concat(civil);
+                id.unshift(civil.length);
+                id.unshift(killers.length);
+                idString = id.join();
+                inputTimes ++;
 
             }
             else{
@@ -33,8 +61,9 @@ function getNumber(){
             alert("请输入4~18的整数");
             number.value = 8;
         }
-    }
 
+
+    }
 }
 
 function allocate(a){
@@ -62,25 +91,10 @@ function    shuffle(x,array){
 
 function submit(){     //点击"去发牌按钮",根据杀手和水民人数分配角色,跳转到查看身份页面,且传递角色参数字符串到身份页面
     var button = document.getElementsByClassName("submit");
-    var civil = []; //建立玩家数组
-    var killers = []; //建立杀手数组,这个数组存放杀手玩家的序号. 另一种方式是建立一个数组,把"杀手""平民"设置为
-                      // 数组元素的值,然后打乱序号. 在使用上更简洁一些.
-    var number = document.getElementById("input");
-    num = number.value;
-    var killerNum = Math.floor(num/4);
-    for(var i = 1;i <= num; i++){
-        civil.push(i);
-    }
-    killers = shuffle(killerNum,civil);
-    console.log(killers);
-    console.log(civil);
-    var id = killers.concat(civil);
-    id.unshift(civil.length);
-    id.unshift(killers.length);
-    var idString =id.join();
+    getNumber();
     
     button[0].onclick = function(){
         
-        window.location.href = "3-checkId-1.html"+"?index="+"1,"+idString;  //传递参数过去,不知道为啥非得加index=，但不加不行，会出现奇怪的东西
+        window.location.href = "3-checkId-1.html"+"?index="+"1,"+idString;  //传递参数过去,不知道为啥非得加index=，可能是一种格式吧,不加不行，会出现奇怪的东西
     }                                                              // url里的参数数组,参数组依次是玩家序号,杀手人数,平民人数,杀手序号,平民序号
 }
